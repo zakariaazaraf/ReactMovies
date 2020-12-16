b698cca390489ed791c87004fb1ea2c4036c19c8
zakaria ben salek <zakariaazaraf@gmail.com>
2020-12-17 00:13:01 +0100

    display movies as i wanted them to be

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import axios from './axios' // this is an alias of instance, not axios
-const imageUrl = 'https://image.tmdb.org/t/p/w500'
+const imageUrl = 'https://image.tmdb.org/t/p/original'
 
 function Row({title, fetchUrl}){
     const [movies, setMovies] = useState([])
@@ -20,10 +20,10 @@ function Row({title, fetchUrl}){
 
     console.log(movies)
     return (
-        <div className='-fluid'>
+        <div className='container-fluid'>
             <h2>{title}</h2>
-            <div className='posts'>
-                {movies.map(item => <img className='post' src={`${imageUrl}${item.backdrop_path}`} alt={item.title}></img>)}
+            <div className='row justify-content-center'>
+                {movies.map(item => <div className='col-12 col-sm-4 col-md-3 col-lg-2 mx-1 my-1'><img className='post' src={`${imageUrl}${item.poster_path}`} alt={item.title}></img></div>)}
             </div>
         </div>
     );
